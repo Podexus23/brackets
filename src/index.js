9c6ba18317f9e9c1e55f38b219b03a4eb03bcd48
Podexus23 <santon23@gmail.com>
2021-02-01 00:45:04 +0300

    feat: recursion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brackets){
+ if(str.length == 0){
+   return true
+ }
+ for(let i = 0;i<str.length; i++){
+   for(let bracket of brackets){
+     if(bracket[0] == str[i] && bracket[1] == str[i+1]){
+       let stringo = str.slice(0, i) + str.slice(i+2)
+       return check(stringo, brackets)
+     }
+   }
+ }
+
+ return false
 }
